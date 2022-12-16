9f9b0868e1ad6c5334c07424401496be00352e25
Francesco Corsello <francesco.corsello@gmail.com>
2022-12-16 20:19:04 +0100

    closures_1 v1

--- exercise.js
+++ exercise.js
@@ -1,1 +1,12 @@
-let helloName = "Hello John";
+function printName(){
+    let helloName = "Hello John";
+    function inner(){
+        return helloName;
+    }
+    return inner;
+}
+console.log(printName()());
+
+
+
+
