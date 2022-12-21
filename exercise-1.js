34a01d231e73271e0504bd2f73f83602d634c903
Francesco Corsello <francesco.corsello@gmail.com>
2022-12-21 17:21:55 +0100

    initial commit

--- exercise-1.js
+++ exercise-1.js
@@ -5,8 +5,11 @@ const person1 = {
 };
 
 const person2 = person1;
+person2.firstName = "Simon";
 
 // Modify the property `firstName` of the `person2` in "Simon".
+// we are making a Shallow copy so both variables will reference the same object in the memory
+// that's why, by modifying the object `person2`, also the object `person1` would be modified.
 
 console.log(person1);
 console.log(person2);
