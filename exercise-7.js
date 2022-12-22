1f63c4829f880744bcf3812e84c8238adaf9ff6a
Francesco Corsello <francesco.corsello@gmail.com>
2022-12-22 17:52:26 +0100

    initial commit

--- exercise-7.js
+++ exercise-7.js
@@ -1,5 +1,9 @@
-function sumUntil(maxValue) {
-  // ...
+function sumUntil(maxValue) { 
+  let num = 0;
+  for (let i = 1; i <= maxValue; i++) {
+        num += i;
+  }
+  return num
 }
-
 console.log(sumUntil(5));
+
