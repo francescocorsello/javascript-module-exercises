12c79626b8907fbc60012210978810cbc7f767cd
Francesco Corsello <francesco.corsello@gmail.com>
2022-12-27 17:16:24 +0100

    initial commit

--- exercise-2.js
+++ exercise-2.js
@@ -1,5 +1,18 @@
-const order = {};
+const order = {
+  customer : {
+    address: {
+      city: ""
+    }
+  }
+};
 
-if (order && order.customer && order.customer.address && !order.customer.address.city) {
-  console.log('City is required');
+// if (order && order.customer && order.customer.address && !order.customer.address.city) {
+//   console.log('City is required');
+// }
+
+
+if (!order.customer.address?.city) {
+  console.log("City is required");
 }
+
+
