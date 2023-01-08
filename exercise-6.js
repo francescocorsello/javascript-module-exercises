82bb4d611273a66ceafc4a180b61c9b9631f6d70
Francesco Corsello <francesco.corsello@gmail.com>
2023-01-08 16:28:53 +0100

    initial commit

--- exercise-6.js
+++ exercise-6.js
@@ -18,7 +18,16 @@ class Circle {
 }
 
 class AreaCalculator {
-  // ...
+  static calculate(figure){
+    if (figure instanceof Square) {
+      figure =  square.side*square.side; 
+    } else if (figure instanceof Rectangle) {
+      figure = rectangle.width*rectangle.height;
+    } else {
+      figure =  circle.radius*3,14;
+    }
+    return figure;
+  } 
 }
 
 const square = new Square(4);
@@ -28,3 +37,6 @@ const circle = new Circle(5);
 console.log(AreaCalculator.calculate(square));
 console.log(AreaCalculator.calculate(rectangle));
 console.log(AreaCalculator.calculate(circle));
+
+
+
