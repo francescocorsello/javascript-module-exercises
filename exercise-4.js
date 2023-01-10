55bd95c205063ff281e5f309fb92d9349b5a0be4
Francesco Corsello <francesco.corsello@gmail.com>
2023-01-10 19:30:42 +0100

    initial commit

--- exercise-4.js
+++ exercise-4.js
@@ -1,6 +1,12 @@
-function sum(num1, num2, num3) {
-  return num1 + num2 + num3;
+// function sum(num1, num2, num3) {
+//   return num1 + num2 + num3;
+// }
+
+function sum(...rest) {
+  let total = rest.reduce((a,b) => a + b);
+    return  total;
 }
 
-const numbers = [1, 2, 3];
+
+const numbers = [1, 2, 3,];
 console.log(sum(numbers[0], numbers[1], numbers[2]));
