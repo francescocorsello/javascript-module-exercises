b53e98d99aa86ffac4c2e8c7bda778f44a2ee2a6
Francesco Corsello <francesco.corsello@gmail.com>
2023-01-10 19:07:35 +0100

    initial commit

--- exercise-2.js
+++ exercise-2.js
@@ -1,15 +1,20 @@
 const person = {
   id: 1,
+  info:{
   firstName: 'Mario',
   lastName: 'Rossi',
   age: 25
+}
 };
 
+// const id = person.id;
+// const personInfo = {
+//   firstName: person.firstName,
+//   lastName: person.lastName,
+//   age: person.age
+// };
+
 const id = person.id;
-const personInfo = {
-  firstName: person.firstName,
-  lastName: person.lastName,
-  age: person.age
-};
+const personInfo = {...person.info};
 
 console.log(id, personInfo);
