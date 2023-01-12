88182fec21e2c532940cafd323864b21958fc465
Francesco Corsello <francesco.corsello@gmail.com>
2023-01-12 15:26:08 +0100

    initial commit

--- exercise-2.js
+++ exercise-2.js
@@ -1,3 +1,5 @@
+//Create the `fromJson` method that takes in a json as parameter and returns an object of type `Person`.
+
 class Person {
   constructor(id, firstName, lastName, age) {
     this.id = id;
@@ -9,8 +11,14 @@ class Person {
   toJson() {
     return JSON.stringify(this);
   }
+  
+  static fromJson(el){
+    return JSON.parse(el);
+  }
+  
 }
 
 const json = '{"id":1,"firstName":"Mario","lastName":"Rossi","age":25}';
 const developer = Person.fromJson(json);
 console.log(developer);
+
