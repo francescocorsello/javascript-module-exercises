ac890cf41becd36c2815431be0d05d7116ef8954
Francesco Corsello <francesco.corsello@gmail.com>
2023-01-18 12:59:04 +0100

    initial commit

--- exercise.js
+++ exercise.js
@@ -1,1 +1,38 @@
+// Write a first `promise` that takes as parameter the variable `isLogged` found in the `exercise.js` file.
+// If the variable is true, then we return a random number from the resolve, otherwise we dispatch an error.
+
+// We write a second `promise` that takes a variable of type number as a parameter. If the input parameter is greater than 0.5, in the resolve we must return the following data: `{name: "John", age: 24}`, otherwise we must dispatch an `error`.
+// Once this is done, try to chain the promises to eventually return the final object `{name: "John", age: 24}`
+
 const isLogged = true;
+
+function userLogged(logStatus){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let rand = Math.random();
+            if (logStatus == true) {
+                resolve(rand); 
+            } else {
+                reject(new Error("LogIn Error!"));
+            }
+        }, 500)
+    })
+}
+
+function secondPromise(rand) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (rand > 0.5) {
+                resolve(`{name: "John", age: 24}`)
+            } else {
+                reject(new Error("Error!"));
+            }
+        }, 500);   
+    })  
+}
+    
+userLogged(isLogged)
+    .then(secondPromise)
+    .then((val) => console.log(val))
+    .catch((err) => console.error(err))
+
